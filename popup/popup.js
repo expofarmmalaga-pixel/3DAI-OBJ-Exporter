61c4673f2376419a135f4bf213f5209f0b015ea0
expofarmmalaga-pixel <expofarmmalaga@gmail.com>
2026-07-11 20:26:06 +0200

    popup.js

--- popup/popup.js
+++ popup/popup.js
@@ -1,9 +1,28 @@
+document.getElementById("export").onclick = async () => {
 
-document.getElementById('r').onclick=async()=>{
- let [tab]=await chrome.tabs.query({active:true,currentWindow:true});
- let [res]=await chrome.scripting.executeScript({
- target:{tabId:tab.id},
- func:()=>window.__WebGLExtractor?window.__WebGLExtractor.stats:null
- });
- document.getElementById('o').textContent=JSON.stringify(res.result,null,2);
+    const [tab] = await chrome.tabs.query({
+        active: true,
+        currentWindow: true
+    });
+
+    chrome.scripting.executeScript({
+
+        target: { tabId: tab.id },
+
+        files: [
+            "export/export.js"
+        ]
+
+    }, () => {
+
+        chrome.scripting.executeScript({
+
+            target: { tabId: tab.id },
+
+            func: () => window.exportOBJ()
+
+        });
+
+    });
+
 };
