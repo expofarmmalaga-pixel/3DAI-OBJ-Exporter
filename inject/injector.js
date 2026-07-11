13287e90d1e29c1e5dcd6fda96cb5253544a2273
expofarmmalaga-pixel <expofarmmalaga@gmail.com>
2026-07-11 20:39:52 +0200

    injector.js

--- inject/injector.js
+++ inject/injector.js
@@ -1,7 +1,19 @@
+(() => {
 
-(()=>{
- const s=document.createElement('script');
- s.src=chrome.runtime.getURL('inject/capture.js');
- (document.head||document.documentElement).appendChild(s);
- s.onload=()=>s.remove();
+    function inject(file) {
+
+        const s = document.createElement("script");
+
+        s.src = chrome.runtime.getURL(file);
+
+        (document.head || document.documentElement).appendChild(s);
+
+        s.onload = () => s.remove();
+
+    }
+
+    inject("inject/capture.js");
+
+    inject("export/export.js");
+
 })();
